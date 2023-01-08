77c1d8e22227ebcfa9f90325312fd53b1730cd0e
Tengku rafi <t.mrr39@gmail.com>
2023-01-08 20:51:20 +0700

    new file:   example-app/public/css/style.css new file:   example-app/resources/views/about.blade.php new file:   example-app/resources/views/home.blade.php modified:   example-app/routes/web.php

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Tengku Muhammad Rafi Rahardiansyah",
+        "email" => "t.mrr39@gmail.com"
+    ]);
 });

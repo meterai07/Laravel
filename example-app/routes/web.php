3db235e6c8ad1694cca918e6cd8e4ae6d7d29c1f
Tengku rafi <t.mrr39@gmail.com>
2023-01-10 08:31:18 +0700

    modified:   example-app/public/css/style.css modified:   example-app/resources/views/layouts/main.blade.php modified:   example-app/resources/views/partials/navbar.blade.php new file:   example-app/resources/views/post.blade.php modified:   example-app/resources/views/posts.blade.php modified:   example-app/routes/web.php

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -14,16 +14,66 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Tengku Muhammad Rafi Rahardiansyah",
         "email" => "t.mrr39@gmail.com"
     ]);
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Tengku Muhammad Rafi Rahardiansyah",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique labore esse accusamus suscipit eum aperiam repellat maiores, aut earum a fugiat mollitia voluptas eos beatae officia veritatis iusto nihil ipsum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos soluta sint id ut ipsum eveniet maiores nam nemo esse! Iure!." 
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Yogi Puji Sastomo",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique labore esse accusamus suscipit eum aperiam repellat maiores, aut earum a fugiat mollitia voluptas eos beatae officia veritatis iusto nihil ipsum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos soluta sint id ut ipsum eveniet maiores nam nemo esse! Iure!." 
+        ]
+    ];
+
+    return view('posts', [
+        "title" => "Blog",
+        "posts" => $blog_posts
+    ]);
+});
+
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Tengku Muhammad Rafi Rahardiansyah",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique labore esse accusamus suscipit eum aperiam repellat maiores, aut earum a fugiat mollitia voluptas eos beatae officia veritatis iusto nihil ipsum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos soluta sint id ut ipsum eveniet maiores nam nemo esse! Iure!." 
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Yogi Puji Sastomo",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique labore esse accusamus suscipit eum aperiam repellat maiores, aut earum a fugiat mollitia voluptas eos beatae officia veritatis iusto nihil ipsum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos soluta sint id ut ipsum eveniet maiores nam nemo esse! Iure!." 
+        ]
+    ];
+
+    $current_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $current_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $current_post
+    ]);
 });

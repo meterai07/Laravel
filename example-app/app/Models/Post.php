182ba5deaacfe852b19ba85603bb5a285ad3befa
Tengku rafi <t.mrr39@gmail.com>
2023-01-13 18:50:02 +0700

    modified:   example-app/app/Http/Controllers/PostController.php 	modified:   example-app/app/Models/Post.php 	new file:   example-app/app/Models/Post_.php 	new file:   example-app/database/migrations/2023_01_13_110855_create_posts_table.php 	modified:   example-app/resources/views/post.blade.php 	modified:   example-app/resources/views/posts.blade.php 	modified:   example-app/routes/web.php

--- example-app/app/Models/Post.php
+++ example-app/app/Models/Post.php
@@ -2,33 +2,13 @@
 
 namespace App\Models;
 
-class Post 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
 {
-    private static $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Tengku Muhammad Rafi Rahardiansyah",
-            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique labore esse accusamus suscipit eum aperiam repellat maiores, aut earum a fugiat mollitia voluptas eos beatae officia veritatis iusto nihil ipsum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos soluta sint id ut ipsum eveniet maiores nam nemo esse! Iure!." 
-        ],
-        [
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Yogi Puji Sastomo",
-            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique labore esse accusamus suscipit eum aperiam repellat maiores, aut earum a fugiat mollitia voluptas eos beatae officia veritatis iusto nihil ipsum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos soluta sint id ut ipsum eveniet maiores nam nemo esse! Iure!." 
-        ]
-    ];
+    use HasFactory;
 
-    public static function all()
-    {
-        return collect(self::$blog_posts);
-    }
-
-    public static function find($slug)
-    {
-        $posts = static::all();
-        $current_post = $posts->firstWhere('slug', $slug);
-
-        return $current_post;
-    }
+    protected $fillable = ['title', 'slug', 'excerpt', 'body'];
+    protected $guarded = ['id'];
 }
---
+++ example-app/app/Models/Post_.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+class Post 
+{
+    private static $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Tengku Muhammad Rafi Rahardiansyah",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique labore esse accusamus suscipit eum aperiam repellat maiores, aut earum a fugiat mollitia voluptas eos beatae officia veritatis iusto nihil ipsum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos soluta sint id ut ipsum eveniet maiores nam nemo esse! Iure!." 
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Yogi Puji Sastomo",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Similique labore esse accusamus suscipit eum aperiam repellat maiores, aut earum a fugiat mollitia voluptas eos beatae officia veritatis iusto nihil ipsum. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos soluta sint id ut ipsum eveniet maiores nam nemo esse! Iure!." 
+        ]
+    ];
+
+    public static function all()
+    {
+        return collect(self::$blog_posts);
+    }
+
+    public static function find($slug)
+    {
+        $posts = static::all();
+        $current_post = $posts->firstWhere('slug', $slug);
+
+        return $current_post;
+    }
+}
